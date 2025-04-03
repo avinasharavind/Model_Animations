c9185b5ce6eae7b5c1834d652a708f4e7b462de0
avinasharavind <aviaravind15@gmail.com>
2025-04-02 23:07:57 -0400

    Fully condensed RTMA plotting
    	modified:   model_rtma/helpers.py
    	modified:   model_rtma/rtma_animations.py
    	deleted:    model_rtma/rtmapcp.py
    	deleted:    model_rtma/rtmatemp.py
    	deleted:    model_rtma/rtmawind.py
    	modified:   test.ipynb

--- model_rtma/helpers.py
+++ model_rtma/helpers.py
@@ -20,3 +20,47 @@ def tempHelp(temp):
 
 def tempNorm():
     return paint.make_custom_cmaps(paint.NWSTemperature.name, paint.NWSTemperature.colors, ((paint.NWSTemperature.bounds*2)+30)*.75)[0]
+
+def windHelp(wind):
+    return wind.i10fg*2
+
+def windNorm():
+    colors = paint.NWSWindSpeed.colors
+    norm, norm2 = paint.make_custom_cmaps("customwind", colors, paint.NWSWindSpeed.bounds*1.5)
+    return norm2
+
+def windCmap():
+    norm2 = windNorm()
+    return plt.get_cmap("customwind")
+
+def analysis_mapfix(ax):
+    ax = ax.LAND(edgecolor="k", linewidth=1)
+    ax = ax.COUNTIES(edgecolor="k", linewidth=0.5)
+    ax = ax.BORDERS(color="#aab9d1", linewidth=1)
+    ax = ax.STATES(edgecolor="k", linewidth=1)
+    ax = ax.LAKES(facecolor="#ffffff00", linewidth=0.5)
+    ax = ax.ax
+    return ax
+
+def precipHelp(precip):
+    return precip.tp/25.4
+
+def precipNorm():
+    colors = paint.NWSPrecipitation.colors
+    colors[0] = "#1b243300"
+    norm, norm2 = paint.make_custom_cmaps("customprecip", colors, paint.NWSPrecipitation.bounds/25)
+    return norm2
+
+def precipCmap():
+    norm2 = precipNorm()
+    return plt.get_cmap("customprecip")
+
+def precip_mapfix(ax):
+    ax = ax.LAND(facecolor="#aab9d1", edgecolor="k", linewidth=1)
+    ax = ax.COUNTIES(edgecolor="k", linewidth=0.5)
+    ax = ax.BORDERS(color="#aab9d1", linewidth=1)
+    ax = ax.STATES(facecolor="#aab9d1", edgecolor="k", linewidth=1)
+    ax = ax.LAKES(facecolor="#1b243300", linewidth=0.5)
+    ax = ax.OCEAN(facecolor="#1b2433")
+    ax = ax.ax
+    return ax
--- model_rtma/rtma_animations.py
+++ model_rtma/rtma_animations.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from PIL import Image
 import warnings
-import time
+import os
 
 import helpers
 
@@ -19,18 +19,22 @@ rcParams["font.weight"] = "bold"
 
 """
 Varidict holds variable information in the following format:
-[Herbie Search Key, Conversion Function, cmap, norm, cbar label, ticklabels]
+[Herbie Search Key, Conversion Function, cmap, norm, cbar label, ticklabels, product, mapfix]
 """
 varidict = {
-    "temp": [":TMP:2 m above ground:anl", helpers.tempHelp, paint.NWSTemperature.cmap2, helpers.tempNorm, "2m Temperature (" + r"$\degree$F" + ")", (((paint.NWSTemperature.bounds*2)+30)*.75)[1::4]], 
+    "temp": [":TMP:2 m above ground:anl", helpers.tempHelp, paint.NWSTemperature.cmap2, helpers.tempNorm, "2m Temperature (" + r"$\degree$F" + ")", (((paint.NWSTemperature.bounds*2)+30)*.75)[1::4].astype(int), "anl", helpers.analysis_mapfix], 
+    "wind": [":GUST:10 m above ground:anl", helpers.windHelp, helpers.windCmap(), helpers.windNorm, "10m Wind Gusts (" + r"mph" + ")", (paint.NWSWindSpeed.bounds*1.5)[1::2].astype(int), "anl", helpers.analysis_mapfix],
+    "precip": [None, helpers.precipHelp, helpers.precipCmap(), helpers.precipNorm, "Hourly Precipitation (" + r"in" + ")", (paint.NWSPrecipitation.bounds/25)[1::2], "pcp", helpers.precip_mapfix],
     }
 
 class realTime:
     """
-    Class realTime initalizes a realTime object for easy animation.
+    Class realTime initalizes a realTime object for easy RTMA plotting and animation.
+    The code is... not what I'd consider well written. But it works! And that's what matters, isn't it.
     """
-    def __init__(self, d, init):
+    def __init__(self, m, d, init, title="def"):
         self.model = "rtma"
+        self.m = m
         self.d = d
         self.i = init
 
@@ -45,7 +49,15 @@ class realTime:
         self.ax = None
         self.p = None
         self.var = None
-        
+        self.product = None
+        self.mapfix = None
+        self.spacing = None
+
+        if title=="def":
+            self.title = self.ds.time.dt.strftime('%b %d, %Y').item()
+        else:
+            self.title = title
+
         self.frames = []
 
     def variable(self, var):
@@ -62,11 +74,20 @@ class realTime:
         self.norm = varinfo[3]
         self.label = varinfo[4]
         self.ticks = varinfo[5]
+        self.product = varinfo[6]
+        self.mapfix = varinfo[7]
+
+        if self.product=="anl":
+            self.spacing="proportional"
+        else:
+            self.spacing="uniform"
 
     def plot(self):
         if self.i<10:
             self.i = f"0{self.i}"
-        H = Herbie(f"2025-04-{self.d} {self.i}:00", model="rtma", product="anl")
+        if self.m<10:
+            self.m = f"0{self.m}"
+        H = Herbie(f"2025-{self.m}-{self.d} {self.i}:00", model=self.model, product=self.product)
 
         self.H = H
 
@@ -75,12 +96,7 @@ class realTime:
 
         plt.clf()
         ax = EasyMap("50m", crs=self.ds.herbie.crs, figsize=[7, 7], dpi=400, add_coastlines=True, coastlines_kw={"color":"#1b2433"})
-        ax = ax.LAND(edgecolor="k", linewidth=1)
-        ax = ax.COUNTIES(edgecolor="k", linewidth=0.5)
-        ax = ax.BORDERS(color="#aab9d1", linewidth=1)
-        ax = ax.STATES(edgecolor="k", linewidth=1)
-        ax = ax.LAKES(facecolor="#ffffff00", linewidth=0.5)
-        ax = ax.ax
+        ax = self.mapfix(ax)
 
         p = ax.pcolormesh(
             self.ds.longitude,
@@ -92,7 +108,7 @@ class realTime:
         )
 
         ax.set_title(
-            f"{self.ds.time.dt.strftime('%b %d, %Y').item()}\nReal-Time Mesoscale Analysis",
+            f"{self.title}\nReal-Time Mesoscale Analysis",
             loc="left",
             color="#e1e8f2",
             size=14
@@ -120,45 +136,56 @@ class realTime:
         ax=ax,
         orientation="horizontal",
         pad=0.01,
-        label="2m Temperature (" + r"$\degree$F" + ")",
-        spacing="proportional", 
+        spacing=self.spacing, 
         ticks=self.ticks,
         )
 
         cb.ax.tick_params(color="#e1e8f2")
         cb.ax.set_xlabel(self.label, color="#e1e8f2", size=18)
-        cb.ax.set_xticklabels(self.ticks.astype(int), color="#e1e8f2", size=14)
+        cb.ax.set_xticklabels(self.ticks, color="#e1e8f2", size=14)
         cb.outline.set_visible(False)
 
-        name = f"images/frame{self.i}d{self.d}"
+        name = f"images/d{self.d}frame{self.i}"
         plt.savefig(name, dpi=400, bbox_inches="tight", pad_inches=0.3)
         self.frames.append(name)
     
-    def new_frame(self, i, d):
+    def new_frame(self, m, d, i):
+        self.m = m
         self.i = i
         self.d = d
         self.plot()
 
     def create_gif(self):
         images = [Image.open(f"{image_path}.png") for image_path in self.frames]
-        # Save as GIF
         images[0].save(
         f"gifs/rtma_{self.var}.gif",
         save_all=True,
         append_images=images[1:],
         duration=500,
-        loop=0 # 0 means infinite loop
+        loop=0
         )
 
         print(f"GIF created and saved!")
 
-rtma = realTime(1, 12)
-rtma.variable("temp")
+    def clear_folder(self):
+        folder_path = "images"
 
-d=1
-for i in range(12,24):
-    rtma.new_frame(i, d)
+        for item in os.scandir(folder_path):
+            if item.is_file():
+                os.remove(item.path)
+
+rtma = realTime(3, 29, 12, "Backdoor Cold Front")
+rtma.variable("precip")
+
+m=3
+d=29
+for i in range(20,24):
+    rtma.new_frame(m,d,i)
+d=30
+for i in range(0,2):
+    rtma.new_frame(m,d,i)
 
 rtma.create_gif()
+rtma.clear_folder()  #Comment out to leave frames in folder
 
         
